3f7fbfba91d4528a3bd325df65a8601e68d24bf9
Mohiul Islam <mohiul.islm@hotmail.com>
2016-04-08 08:29:02 -0400

    Commit before changing State Transition Table

--- LexicalAnalyzer/src/lex/Main.java
+++ LexicalAnalyzer/src/lex/Main.java
@@ -1,8 +1,10 @@
 package lex;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
@@ -11,11 +13,37 @@ public class Main {
 	private static LexicalAnalyzer lex; 
 
 	public static void main(String[] args) throws IOException {
-		lex = new LexicalAnalyzer("errors.txt");
-		lex.handleFile("test-program.txt");
+		
+		//input file name
+		String programFileName = "test-program.txt";
+		System.out.print("Please enter input program file name [test-program.txt]:");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inputFile = br.readLine();
+		if(inputFile.length() > 0){
+			programFileName = inputFile;
+		}
+		
+		//token file name
+		String tokenFileName = "tokens.txt";
+		System.out.print("Please enter output token file name [tokens.txt]:");
+		inputFile = br.readLine();
+		if(inputFile.length() > 0){
+			tokenFileName = inputFile;
+		}
+		
+		//error file name
+		String errorFileName = "errors.txt";
+		System.out.print("Please enter output error file name [errors.txt]:");
+		inputFile = br.readLine();
+		if(inputFile.length() > 0){
+			errorFileName = inputFile;
+		}
+		
+		lex = new LexicalAnalyzer(errorFileName);
+		lex.handleFile(programFileName);
 		Writer writer = new BufferedWriter(
 				new OutputStreamWriter(
-				new FileOutputStream("tokens.txt"), "utf-8"));
+				new FileOutputStream(tokenFileName), "utf-8"));
 		Token token = lex.getNextToken();
 		while(token != null){
 			String tokenStr = token.toString();
